7b4febb0bb1a656989eb31356c22656481b9537b
zhanglin <zhanglin00129@gmail.com>
2014-11-13 15:26:18 +0800

    input some different non-promotional barcodes

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -4,27 +4,30 @@
 */
 function printInventory(inputs){
   var allItems = loadAllItems();
-  var detailList;
-  for(var i=0;i<allItems.length;++i){
-     if(allItems[i].barcode == inputs){
-       detailList = allItems[i];
-       detailList.num = 1;
-     }
+  var detailList = {};
+  for(var j=0;j<inputs.length;j++){
+    for(var i=0;i<allItems.length;++i){
+       if(allItems[i].barcode == inputs[j]){
+           detailList[j] = allItems[i];
+         detailList[j].num = 1;
+       }
+    }
   }
 
-    var printInfo ='***<没钱赚商店>购物清单***\n' ;
-    if(detailList){
-      printInfo += '名称：'+ detailList.name +'，数量：'+detailList.num+detailList.unit+
-        '，单价：'+detailList.price.toFixed(2)+'(元)，小计：'+
-        (detailList.num*detailList.price).toFixed(2)+'(元)\n' ;
-    }
-
-    printInfo +=
+  var sumPrice = 0;
+  var printInfo ='***<没钱赚商店>购物清单***\n' ;
+  for(var i in detailList){
+    printInfo += '名称：'+ detailList[i].name +'，数量：'+detailList[i].num+detailList[i].unit+
+      '，单价：'+detailList[i].price.toFixed(2)+'(元)，小计：'+
+      (detailList[i].num*detailList[i].price).toFixed(2)+'(元)\n' ;
+    sumPrice += detailList[i].num*detailList[i].price;
+  }
+  printInfo +=
     '----------------------\n' +
     '挥泪赠送商品：\n' +
     '----------------------\n' +
-    '总计：'+(detailList?(detailList.num*detailList.price).toFixed(2):'0.00')+'(元)\n' +
+    '总计：'+sumPrice.toFixed(2)+'(元)\n' +
     '节省：0.00(元)\n' +
     '**********************';
-    console.log(printInfo);
+  console.log(printInfo);
 }
